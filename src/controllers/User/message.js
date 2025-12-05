555552ad7cbe6d68346e6d6e96cbb5057e018b22
Hoai Phong <tranphuhoaiphong@gmail.com>
2025-12-05 22:29:04 +0700

    push

--- src/controllers/User/message.js
+++ src/controllers/User/message.js
@@ -2,8 +2,7 @@ const MessageService = require("../../services/User/message");
 
 class MessageController {
   static async getMessage(req, res) {
-    const {userId} = req.body; 
-    
+    const userId = req.userId;  
     try {
       const reviews = await MessageService.getMessage(userId);
       res.status(200).json(reviews);
