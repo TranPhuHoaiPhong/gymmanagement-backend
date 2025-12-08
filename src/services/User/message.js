b1257ab9ce16f6e0cea1309783039bc47e0edf05
Hieu Pham <hieu02617@gmail.com>
2025-12-08 22:55:02 +0700

    trtr

--- src/services/User/message.js
+++ src/services/User/message.js
@@ -1,57 +1,49 @@
 const Message = require("../../models/Message/Message");
 
 class MessageService {
-
   static async getMessage(userId) {
-  try {
-    const result = await Message.find({
-      $or: [
-        { from: userId },
-        { to: userId }
-      ]
-    }).sort({ timestamp: 1 }); // có thể sắp xếp theo thời gian
+    try {
+      const result = await Message.find({
+        $or: [{ from: userId }, { to: userId }],
+      }).sort({ timestamp: 1 }); // có thể sắp xếp theo thời gian
 
-    return {
-      success: true,
-      data: result,
-    };
-  } catch (error) {
-    throw error;
+      return {
+        success: true,
+        data: result,
+      };
+    } catch (error) {
+      throw error;
+    }
   }
-}
-
 
   static async getAllMessage() {
-  try {
-    // Lấy tất cả message, loại bỏ from = adminId
-    const adminId = "68ff36d578fc9208ee291a83"; 
-    const result = await Message.find({ from: { $ne: adminId } });
+    try {
+      // Lấy tất cả message, loại bỏ from = adminId
+      const adminId = "68ff36d578fc9208ee291a83";
+      const result = await Message.find({ from: { $ne: adminId } });
 
-    // Đếm số lần xuất hiện của từng 'from'
-    const countFrom = {};
-    result.forEach(item => {
-      const fromId = item.from.toString();
-      if (countFrom[fromId]) {
-        countFrom[fromId]++;
-      } else {
-        countFrom[fromId] = 1;
-      }
-    });
+      // Đếm số lần xuất hiện của từng 'from'
+      const countFrom = {};
+      result.forEach((item) => {
+        const fromId = item.from.toString();
+        if (countFrom[fromId]) {
+          countFrom[fromId]++;
+        } else {
+          countFrom[fromId] = 1;
+        }
+      });
 
-    console.log("Kết quả đếm từ 'from' (bỏ admin):", countFrom);
+      console.log("Kết quả đếm từ 'from' (bỏ admin):", countFrom);
 
-    return {
-      success: true,
-      data: result,
-      countFrom,
-    };
-  } catch (error) {
-    throw error;
+      return {
+        success: true,
+        data: result,
+        countFrom,
+      };
+    } catch (error) {
+      throw error;
+    }
   }
-}
-
-
-  
 }
 
 module.exports = MessageService;
